039d612ad6996e8f882405cc53de500bf6325237
slevkor <levk3112@gmail.com>
2016-09-09 13:25:57 +0300

    Unity app added
    WUUPIIDUUU

---
+++ UnityVRGipProj/Assets/Scripts/ColorScript.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ColorScript : MonoBehaviour {
+		private AndroidJavaClass ajc;
+		void Start () {
+			//OVRTouchpad.Create ();
+			//OVRTouchpad.TouchHandler += OVRTouchpad_TouchHandler;
+			//textObject = GameObject.Find("TryText").GetComponent<TextMesh>();
+
+		ajc = new AndroidJavaClass("com.GIP.VRAPP.UnityTalk"); 
+
+		}
+
+		void OVRTouchpad_TouchHandler (object sender, System.EventArgs e)
+		{
+			//OVRTouchpad.TouchArgs touchArgs = (OVRTouchpad.TouchArgs)e;
+			//if (touchArgs.TouchType == OVRTouchpad.TouchEvent.SingleTap) {
+			//	ShouldMoveToSecondScene = true;
+			//}
+		}
+
+		// Update is called once per frame
+		void Update () {
+		int r = 0;
+		int g = 0;
+		int b = 0;
+		if (ajc != null) {
+			r = ajc.CallStatic<int> ("getR");
+			g = ajc.CallStatic<int> ("getG");
+			b = ajc.CallStatic<int> ("getB");
+		}
+			
+			//textObject.text = r.ToString() + " " + g.ToString() + " " + b.ToString() ;
+			GameObject.Find("Sphere").GetComponent<Renderer>().material.color = 
+				new Color(realRGBtoUnityRGB(r),realRGBtoUnityRGB(g),realRGBtoUnityRGB(b));
+		if(Input.GetButton("Tap"))
+		{
+			MoveToSecondSceneSequence (r, g, b);
+		}
+		if (Input.touches.Length > 0 && Input.GetTouch(0).tapCount > 0) {
+			MoveToSecondSceneSequence (r, g, b);
+			}
+		}
+
+	public void MoveToSecondSceneSequence(int r,int g , int b) {
+		MovingBallScript.ballColor = new Color (realRGBtoUnityRGB (r), realRGBtoUnityRGB (g), realRGBtoUnityRGB (b));
+		ajc.CallStatic("moveToScene2");
+		SceneManager.LoadScene ("MainScene");
+		}
+
+		public float realRGBtoUnityRGB(int color){
+			return ((float)color / 255);
+		}
+	}
---
+++ UnityVRGipProj/Assets/Scripts/MovingBallScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingBallScript : MonoBehaviour {
+	private RuntimePlatform platform = Application.platform;
+	public GameObject particle;
+	public float speed;
+	private Vector3 target;
+	static public Color ballColor;
+	private AndroidJavaClass ajc;
+	void Start(){
+		target = transform.position;
+		ajc = new AndroidJavaClass("com.GIP.VRAPP.UnityTalk"); 
+		if (ballColor != null) {
+			GameObject.Find ("MovingSphere").GetComponent<Renderer> ().material.color = 
+				new Color ((int)ballColor.r, (int)ballColor.g, (int)ballColor.b);
+		}
+		if (platform == RuntimePlatform.Android || platform == RuntimePlatform.IPhonePlayer) {
+			if (ajc != null) {
+				speed = ajc.CallStatic<int> ("getSpeed");
+			}
+		} else if (platform == RuntimePlatform.WindowsEditor) {
+			speed = 30;
+		}
+	}
+	void Update(){
+		float step = speed * Time.deltaTime;
+		if(platform == RuntimePlatform.Android || platform == RuntimePlatform.IPhonePlayer){
+			if (ajc != null) {
+				int x = (int)(ajc.CallStatic<double> ("getX") * Screen.width);
+				int y = (int)(ajc.CallStatic<double> ("getY") * Screen.height);
+				float dis = (float)ajc.CallStatic<double> ("getDis");
+				Vector3 pos = new Vector3 (x, y, dis);
+				target = Camera.main.ScreenToWorldPoint (pos);
+			}
+		}else if(platform == RuntimePlatform.WindowsEditor || platform == RuntimePlatform.WindowsPlayer){
+			if(Input.GetMouseButtonDown(0)) {
+				Vector3 pos = new Vector3(Input.mousePosition.x, Input.mousePosition.y,10);
+				target = Camera.main.ScreenToWorldPoint(pos);
+			}
+		}
+		transform.position = Vector3.MoveTowards(transform.position,target,step);
+	}
+	public float realRGBtoUnityRGB(int color){
+		return ((float)color / 255);
+	}
+}
+
